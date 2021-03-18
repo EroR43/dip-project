994b281777acde318108b9fc311b832578cba8bc
EroR43 <62690192+EroR43@users.noreply.github.com>
2021-03-18 14:14:42 +0800

    Shoot Update 1
    Re-structuring of how the laser functions .

--- PewPew Proto/Assets/ShipShoot.cs
+++ PewPew Proto/Assets/ShipShoot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ShipShoot : MonoBehaviour
 {
     [SerializeField]
-    private GameObject laserObj;
+    private List<GameObject> laserObjs;
     //Transform laserOrigin;
 
     [SerializeField]
@@ -18,34 +18,38 @@ public class ShipShoot : MonoBehaviour
 
     private void Awake()
     {
-        origTransform = laserObj.transform;
+        foreach (GameObject las in GameObject.FindGameObjectsWithTag("Laser"))
+        {
+            laserObjs.Add(las);
+        }
+        origTransform = laserObjs[0].transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Shoot();
+        //Shoot();
     }
 
     void Shoot()
     {
-        if(cellChrg > 1)
-        {
-            if(Input.GetMouseButton(0) != false)
-            {
-                Vector3 scale = new Vector3();
-                scale.z = 1 * Time.deltaTime;
-                laserObj.transform.localScale += scale;
-            }
-            else if(Input.GetMouseButtonUp(0) == true)
-            {
-                for (float tarPOS = 30; laserObj.transform.position.z < tarPOS;)
-                {
-                    Vector3 pos = new Vector3();
-                    pos.z = 1 * Time.deltaTime;
-                    laserObj.transform.position += pos;
-                }
-            }
-        }
+        //if(cellChrg > 1)
+        //{
+        //    if(Input.GetMouseButton(0) != false)
+        //    {
+        //        Vector3 scale = new Vector3();
+        //        scale.z = 1 * Time.deltaTime;
+        //        laserObj.transform.localScale += scale;
+        //    }
+        //    else if(Input.GetMouseButtonUp(0) == true)
+        //    {
+        //        for (float tarPOS = 30; laserObj.transform.position.z < tarPOS;)
+        //        {
+        //            Vector3 pos = new Vector3();
+        //            pos.z = 1 * Time.deltaTime;
+        //            laserObj.transform.position += pos;
+        //        }
+        //    }
+        //}
     }
 }
